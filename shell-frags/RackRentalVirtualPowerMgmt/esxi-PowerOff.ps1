e3e27df51989068ad38bdd9e5a2e018208c147d4
ReachableCEO <reachableceo@turnsys.com>
2023-11-25 18:44:05 +0000

    Nov 25, 2023, 12:44 PM

---
+++ shell-frags/RackRentalVirtualPowerMgmt/esxi-PowerOff.ps1
@@ -0,0 +1,9 @@
+param(
+[string]$vcenter,
+[string]$vm
+)
+
+Get-Module -ListAvailable PowerCLI* | Import-Module -Verbose:$false > /dev/null
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false > /dev/null
+Connect-ViServer -Server $vcenter -User tplab\powervtps -Password h7x14vFGkJr5 -Verbose:$false > /dev/null
+Stop-VM -RunAsync -VM "$vm" -Confirm:$false -Verbose:$false > /dev/null
